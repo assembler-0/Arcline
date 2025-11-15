ce6911fafc65a09b1faf3baf5d371e67ed9ea377
assembler-0 <diaviekone13@gmail.com>
2025-11-15 15:31:22 +0700

    dtb

---
+++ include/version.h
@@ -0,0 +1,9 @@
+#ifndef _VERSION_H
+#define _VERSION_H
+
+#define KERNEL_VERSION "0.0.1"
+#define KERNEL_BUILD_DATE __DATE__ " " __TIME__
+#define KERNEL_AUTHOR "assembler-0"
+#define KERNEL_NAME "Arcline(R)" " " KERNEL_VERSION
+
+#endif // _VERSION_H
--- init/main.c
+++ init/main.c
@@ -1,11 +1,16 @@
 #include <drivers/serial.h>
 #include <dtb.h>
 
+#include "version.h"
+
 void kmain(void) {
     serial_init();
-    serial_puts("Arcline(R) microkernel\n");
-    serial_puts("copyright (C) 2025 assembler-0\n");
-    
+    serial_puts(KERNEL_NAME);
+    serial_puts("\ncopyright (C) 2025 assembler-0");
+    serial_puts("\nbuild ");
+    serial_puts(KERNEL_BUILD_DATE);
+    serial_puts("\n");
+
     // Initialize and dump DTB info
     dtb_init();
     dtb_dump_info();
